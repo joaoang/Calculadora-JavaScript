ab4a32b5a9ba1f3efffefeb88440f1a3b95d2805
João Angrezani <jvpb1234@hotmail.com>
2023-06-21 16:04:21 -0300

    PRONTA.

--- area.js
+++ area.js
@@ -1,3 +1,24 @@
-function insert () {
+function insert (num) {
+var numero = document.getElementById('visor').innerHTML;
+document.getElementById('visor').innerHTML = numero + num;
+}
+
+function clean () {
+    document.getElementById('visor').innerHTML = "";
+}
+
+function back () {
+    var visor = document.getElementById('visor').innerHTML;
+    document.getElementById('visor').innerHTML = visor.substring(0, visor.length -1)
+}
+
+function calcular () {
     
+    var resultado = document.getElementById('visor').innerHTML;
+    if (resultado) {
+        document.getElementById('visor').innerHTML = eval(resultado);
+    }
+    else {
+        document.getElementById('visor').innerHTML = "Nada a calcular :("
+    }
 }
